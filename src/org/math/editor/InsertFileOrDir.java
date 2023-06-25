8b243f53ee266c3c4eda9f263646bf24d5681cf8
miaomiaodaa <44901307+miaomiaodaa@users.noreply.github.com>
2023-06-25 21:04:49 +0800

    平面的参数方程，确定唯一直线

--- src/org/math/editor/InsertFileOrDir.java
+++ src/org/math/editor/InsertFileOrDir.java
@@ -9,10 +9,10 @@ import java.nio.file.Paths;
 
 public class InsertFileOrDir {
     public static void main(String[] args) {
-        String name = "";
+        String name = "001 命题-确定唯一直线(合集)";
         int begin_num = 0; // 从第N个开始
         int step = 1; // 每次增加或减少step个
-        File dir = new File("E:\\Math\\work_space\\algebra\\005-入门课程-解析几何\\005 点-直线-平面\\008 面-面");
+        File dir = new File("E:\\Math\\work_space\\algebra\\005-入门课程-解析几何\\005 点-直线-平面\\001 直线方程");
         File[] files = dir.listFiles();
         for (int i = 0; i < files.length; i++) {
             File file = files[i];
