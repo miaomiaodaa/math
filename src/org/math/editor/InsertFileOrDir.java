1faf5f4dfb860cdaf522bca449945c882af71bfe
miaomiaodaa <44901307+miaomiaodaa@users.noreply.github.com>
2023-06-26 22:34:49 +0800

    异面直线的公垂线方程

--- src/org/math/editor/InsertFileOrDir.java
+++ src/org/math/editor/InsertFileOrDir.java
@@ -9,17 +9,18 @@ import java.nio.file.Paths;
 
 public class InsertFileOrDir {
     public static void main(String[] args) {
-        String name = "001 命题-确定唯一直线(合集)";
-        int begin_num = 0; // 从第N个开始
-        int step = 1; // 每次增加或减少step个
-        File dir = new File("E:\\Math\\work_space\\algebra\\005-入门课程-解析几何\\005 点-直线-平面\\001 直线方程");
+        String name = "";
+        int begin_num = 9; // 从第N个开始
+        int end_num = 999;
+        int step = -1; // 每次增加或减少step个
+        File dir = new File("E:\\Math\\work_space\\algebra\\005-入门课程-解析几何\\005 点-直线-平面\\006 位置关系-线-线");
         File[] files = dir.listFiles();
         for (int i = 0; i < files.length; i++) {
             File file = files[i];
             String fileName = files[i].getName();
             try {
                 Integer i1 = Integer.valueOf(fileName.substring(0, file.getName().indexOf(" ")));
-                if (i1 > begin_num) {
+                if (i1 > begin_num && i1 < end_num) {
                     int ser = i1 + step;
                     String ii = (ser < 10 ? "00" + (ser) : (ser < 100 ? "0" + (ser) : (ser + "")));
                     String newFileName = file.getParentFile().getCanonicalPath() + File.separator + ii + " " + file.getName().substring(file.getName().indexOf(" ") + 1);
