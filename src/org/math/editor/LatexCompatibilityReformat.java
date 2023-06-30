0b2b056c63c2b88349c4d5907af34959ccbc9f22
miaomiaodaa <44901307+miaomiaodaa@users.noreply.github.com>
2023-06-30 20:18:49 +0800

    关于复数

--- src/org/math/editor/LatexCompatibilityReformat.java
+++ src/org/math/editor/LatexCompatibilityReformat.java
@@ -15,7 +15,8 @@ public class LatexCompatibilityReformat {
         String[] dirs = new String[]{
                 "E:\\Math\\work_space\\algebra\\004-入门课程-线性代数",
                 "E:\\Math\\work_space\\algebra\\003-入门课程-数学分析",
-                "E:\\Math\\work_space\\algebra\\005-入门课程-解析几何"
+                "E:\\Math\\work_space\\algebra\\005-入门课程-解析几何",
+                "E:\\Math\\work_space\\algebra\\002-初等数学"
         };
         for (int i = 0; i < dirs.length; i++) {
             File dir = new File(dirs[i]);
