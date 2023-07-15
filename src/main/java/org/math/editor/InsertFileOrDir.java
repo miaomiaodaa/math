535f5683b3e288ece971b7828139f9a0adee3106
miaomiaodaa <44901307+miaomiaodaa@users.noreply.github.com>
2023-07-15 21:10:18 +0800

    蒙日圆

--- src/main/java/org/math/editor/InsertFileOrDir.java
+++ src/main/java/org/math/editor/InsertFileOrDir.java
@@ -12,10 +12,10 @@ public class InsertFileOrDir {
         String name = ""; // 新文件的文件名
         String add_prefix = ""; // 添加统一前缀
         String del_prefix = ""; // 删除统一前缀
-        int begin_num = 2; // 从第N个开始
+        int begin_num = 1; // 从第N个开始
         int end_num = 999;
-        int step = 1; // 每次增加或减少step个
-        File dir = new File("E:\\Math\\work_space\\algebra\\005-入门课程-解析几何\\008 圆锥曲线\\005 圆锥曲线-双曲线");
+        int step = -2; // 每次增加或减少step个
+        File dir = new File("E:\\Math\\work_space\\algebra\\005-入门课程-解析几何\\008 圆锥曲线");
 
 
         File[] files = dir.listFiles();
