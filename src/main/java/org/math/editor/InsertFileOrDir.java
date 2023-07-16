a3afbc1743033dcb1d2e2e6a5251e9975d5e1bc4
miaomiaodaa <44901307+miaomiaodaa@users.noreply.github.com>
2023-07-16 22:32:17 +0800

    位似变换

--- src/main/java/org/math/editor/InsertFileOrDir.java
+++ src/main/java/org/math/editor/InsertFileOrDir.java
@@ -12,10 +12,10 @@ public class InsertFileOrDir {
         String name = ""; // 新文件的文件名
         String add_prefix = ""; // 添加统一前缀
         String del_prefix = ""; // 删除统一前缀
-        int begin_num = 5; // 从第N个开始
+        int begin_num = 3; // 从第N个开始
         int end_num = 999;
         int step = -1; // 每次增加或减少step个
-        File dir = new File("E:\\Math\\work_space\\algebra\\005-入门课程-解析几何\\011 高等几何");
+        File dir = new File("E:\\Math\\work_space\\algebra\\005-入门课程-解析几何\\011 高等几何\\003 引论-相似变换");
 
 
         File[] files = dir.listFiles();
