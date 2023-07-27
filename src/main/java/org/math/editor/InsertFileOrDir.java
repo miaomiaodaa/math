9bf064e467f683e0766b10b2e7c57dfb2514b38d
miaomiaodaa <44901307+miaomiaodaa@users.noreply.github.com>
2023-07-27 15:02:10 +0800

    回顾-高中知识完善初中知识

--- src/main/java/org/math/editor/InsertFileOrDir.java
+++ src/main/java/org/math/editor/InsertFileOrDir.java
@@ -12,10 +12,10 @@ public class InsertFileOrDir {
         String name = ""; // 新文件的文件名
         String add_prefix = ""; // 添加统一前缀
         String del_prefix = ""; // 删除统一前缀
-        int begin_num = 1; // 从第N个开始
+        int begin_num = 7; // 从第N个开始
         int end_num = 999;
-        int step = 2; // 每次增加或减少step个
-        File dir = new File("E:\\Math\\work_space\\algebra\\005-入门课程-解析几何\\011 圆锥曲线\\002 圆锥曲线-圆-几何篇\\005 圆与圆");
+        int step = 1; // 每次增加或减少step个
+        File dir = new File("E:\\Math\\work_space\\algebra\\005-入门课程-解析几何\\006 角");
 
 
         File[] files = dir.listFiles();
