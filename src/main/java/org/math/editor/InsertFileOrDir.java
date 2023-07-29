f13a50428d7d9f44832535dc244722c8f18a7d4c
miaomiaodaa <44901307+miaomiaodaa@users.noreply.github.com>
2023-07-29 21:34:13 +0800

    回顾-诱导公式Part2

--- src/main/java/org/math/editor/InsertFileOrDir.java
+++ src/main/java/org/math/editor/InsertFileOrDir.java
@@ -12,10 +12,10 @@ public class InsertFileOrDir {
         String name = ""; // 新文件的文件名
         String add_prefix = ""; // 添加统一前缀
         String del_prefix = ""; // 删除统一前缀
-        int begin_num = 4; // 从第N个开始
+        int begin_num = 1; // 从第N个开始
         int end_num = 999;
-        int step = 1; // 每次增加或减少step个
-        File dir = new File("E:\\Math\\work_space\\math\\005-入门课程-解析几何");
+        int step = -6; // 每次增加或减少step个
+        File dir = new File("E:\\Math\\work_space\\math\\005-入门课程-解析几何\\008 三角形\\015 任意角三角函数");
 
 
         File[] files = dir.listFiles();
