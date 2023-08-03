49f6d152ee145fb28db2cc50771deec1d02683c7
miaomiaodaa <44901307+miaomiaodaa@users.noreply.github.com>
2023-08-03 21:04:02 +0800

    回顾-点-线-面位置关系补充

--- src/main/java/org/math/editor/InsertFileOrDir.java
+++ src/main/java/org/math/editor/InsertFileOrDir.java
@@ -15,7 +15,7 @@ public class InsertFileOrDir {
         String name = ""; // 新文件的文件名
         String add_prefix = ""; // 添加统一前缀
         String del_prefix = ""; // 删除统一前缀
-        int begin_num = 2; // 从第N个开始
+        int begin_num = 6; // 从第N个开始
         int end_num = 999; 
         int step = 1; // 每次增加或减少step个
         File dir = new File("E:\\Math\\work_space\\math\\005-入门课程-解析几何\\014 点-直线-平面位置关系\\008 位置关系-面-面");
