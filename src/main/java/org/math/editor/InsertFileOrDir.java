ab2a70803eb72a17b60a647f9afce0e1de9ad210
miaomiaodaa <44901307+miaomiaodaa@users.noreply.github.com>
2023-08-08 00:06:33 +0800

    回顾-圆锥曲线补充-这次条例清晰了

--- src/main/java/org/math/editor/InsertFileOrDir.java
+++ src/main/java/org/math/editor/InsertFileOrDir.java
@@ -15,10 +15,10 @@ public class InsertFileOrDir {
         String name = ""; // 新文件的文件名
         String add_prefix = ""; // 添加统一前缀
         String del_prefix = ""; // 删除统一前缀
-        int begin_num = 14; // 从第N个开始
+        int begin_num = 3; // 从第N个开始
         int end_num = 999; 
-        int step = 1; // 每次增加或减少step个
-        File dir = new File("E:\\Math\\work_space\\math\\005-入门课程-解析几何");
+        int step = 3; // 每次增加或减少step个
+        File dir = new File("E:\\Math\\work_space\\math\\005-入门课程-解析几何\\012 圆锥曲线\\006 圆锥曲线-抛物线\\005 直线与抛物线");
 
 
         Pattern pattern = Pattern.compile("^([0-9]{3}).*$");
