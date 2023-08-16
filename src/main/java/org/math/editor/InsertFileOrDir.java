d933079b56dca595d51324ffba5268a72a2f6612
miaomiaodaa <44901307+miaomiaodaa@users.noreply.github.com>
2023-08-16 23:58:27 +0800

    平面方程

--- src/main/java/org/math/editor/InsertFileOrDir.java
+++ src/main/java/org/math/editor/InsertFileOrDir.java
@@ -15,10 +15,10 @@ public class InsertFileOrDir {
         String name = ""; // 新文件的文件名
         String add_prefix = ""; // 添加统一前缀
         String del_prefix = ""; // 删除统一前缀
-        int begin_num = 3; // 从第N个开始
+        int begin_num = 4; // 从第N个开始
         int end_num = 999; 
         int step = 1; // 每次增加或减少step个
-        File dir = new File("E:\\Math\\work_space\\math\\005-入门课程-解析几何\\008 三角形\\004 角平分线与内心");
+        File dir = new File("E:\\Math\\work_space\\math\\005-入门课程-解析几何\\014 点-直线-平面位置关系\\007 位置关系-线-面");
 
 
         Pattern pattern = Pattern.compile("^([0-9]{3}).*$");
