4add6415c479d859b1e4bf7b3a1b59e9915b7898
miaomiaodaa <44901307+miaomiaodaa@users.noreply.github.com>
2023-08-17 22:11:59 +0800

    线线位置关系

--- src/main/java/org/math/editor/InsertFileOrDir.java
+++ src/main/java/org/math/editor/InsertFileOrDir.java
@@ -15,10 +15,10 @@ public class InsertFileOrDir {
         String name = ""; // 新文件的文件名
         String add_prefix = ""; // 添加统一前缀
         String del_prefix = ""; // 删除统一前缀
-        int begin_num = 2; // 从第N个开始
+        int begin_num = 9; // 从第N个开始
         int end_num = 999; 
-        int step = 1; // 每次增加或减少step个
-        File dir = new File("E:\\Math\\work_space\\math\\005-入门课程-解析几何\\006 直线");
+        int step = -1; // 每次增加或减少step个
+        File dir = new File("E:\\Math\\work_space\\math\\005-入门课程-解析几何\\014 点-直线-平面位置关系\\006 位置关系-线-线");
 
         Pattern pattern = Pattern.compile("^([0-9]{3}).*$");
 
@@ -58,7 +58,7 @@ public class InsertFileOrDir {
                 if (!del_prefix.isEmpty())
                     newFileName = newFileName.replace(del_prefix, "");
                 Path moved = Files.move(Paths.get(file.getCanonicalPath()), Paths.get(newFileName));
-                System.out.println(moved.getFileName());
+                //System.out.println(moved.getFileName());
             } catch (Exception e1) {
                 e1.printStackTrace();
                 System.err.println(fileName);
